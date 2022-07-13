b47d66dd76cb90707f5cfe4b9e373b227dd7f551
ohyuchan <oyuchan50@gmail.com>
2022-07-13 21:53:52 +0900

    중복 코드의 메소드 추출

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -5,11 +5,18 @@ import com.domain.User;
 import java.sql.*;
 
 public class UserDao {
-    public void add(User user) throws ClassNotFoundException, SQLException {
+
+    // 중복된 코드를 독립적인 메소드로 만들어서 중복을 제거했다.
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/myshop"
                 ,"root","ikok9636*");
 
+        return c;
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id,name,password) values (?,?,?)"
         );
@@ -29,10 +36,7 @@ public class UserDao {
     public User get(String id) throws ClassNotFoundException, SQLException {
         User user = new User();
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/myshop","root","ikok9636*"
-        );
+        Connection c = getConnection(); // DB 연결 기능이 필요하면 getConnection() 메소드를 이용하게 된다.
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id=?"
